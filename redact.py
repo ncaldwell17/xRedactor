1ec32252a40333db658413b1d1e17c7a6a2930ac
ncaldwell17 <ncaldwellgatsos@gmail.com>
2020-05-05 03:17:38 -0500

    redaction working

--- redact.py
+++ redact.py
@@ -1,19 +1,101 @@
-class Redact(object):
+import spacy
+nlp = spacy.load('en_core_web_lg')
 
-    def __init__(self, sentence):
-        # need this so the program knows what to highlight for the user
-        self.sentence = arrange_grams(sentence)
-        self.redacted_terms = list()
 
-    # TODO: def arrange_grams(sentence):
-    #   find a library to split into n-grams UNLESS that messes up SpaCy, which would be frustrating.
-    #   this MIGHT BE unnecessary if SpaCy splits a sentence automatically
-    #   pass
+class ClauseRedactor(object):
 
-    # TODO: def redact(sentence):
-    #   self.redacted_terms.append(redact_method_one(sentence)
+    def __init__(self, redactors=None):
+        # self._redactors is a list of redaction methods
+        self._redactors = list()
+        if redactors is not None:
+            self._redactors += redactors
 
-    # TODO: def nerf_orgs(sentence):
-    #   whatever process to find organizations, probably SpaCy
+    def redact(self, clause):
+        for redactor in self._redactors:
+            # NOTE, if you want the redactors to return more things like the actual terms, that needs to be another list
+            clause = redactor(clause)
+        return clause
+
+
+def organization_redactor(string):
+    doc = nlp(string)
+    new_string = string
+    for ent in reversed(doc.ents):
+        if ent.label_ == 'ORG':
+            start = ent.start_char
+            end = start + len(ent.text)
+            new_string = new_string[:start] + "REDACTED" + new_string[end:]
+    return new_string
+
+
+def date_redactor(string):
+    doc = nlp(string)
+    new_string = string
+    for ent in reversed(doc.ents):
+        if ent.label_ == 'DATE':
+            start = ent.start_char
+            end = start + len(ent.text)
+            new_string = new_string[:start] + "REDACTED" + new_string[end:]
+    return new_string
+
+
+def person_redactor(string):
+    doc = nlp(string)
+    new_string = string
+    for ent in reversed(doc.ents):
+        if ent.label_ == 'PERSON':
+            start = ent.start_char
+            end = start + len(ent.text)
+            new_string = new_string[:start] + "REDACTED" + new_string[end:]
+    return new_string
+
+
+def gpe_redactor(string):
+    doc = nlp(string)
+    new_string = string
+    for ent in reversed(doc.ents):
+        if ent.label_ == 'GPE':
+            start = ent.start_char
+            end = start + len(ent.text)
+            new_string = new_string[:start] + "REDACTED" + new_string[end:]
+    return new_string
+
+
+def fac_redactor(string):
+    doc = nlp(string)
+    new_string = string
+    for ent in reversed(doc.ents):
+        if ent.label_ == 'FAC':
+            start = ent.start_char
+            end = start + len(ent.text)
+            new_string = new_string[:start] + "REDACTED" + new_string[end:]
+    return new_string
+
+
+def woa_redactor(string):
+    doc = nlp(string)
+    new_string = string
+    for ent in reversed(doc.ents):
+        if ent.label_ == 'WORK_OF_ART':
+            start = ent.start_char
+            end = start + len(ent.text)
+            new_string = new_string[:start] + "REDACTED" + new_string[end:]
+    return new_string
+
+
+clause_redactor = ClauseRedactor([organization_redactor,
+                                  date_redactor,
+                                  person_redactor,
+                                  gpe_redactor,
+                                  fac_redactor,
+                                  woa_redactor])
+
+example_clause = 'THIS MASTER SERVICES AGREEMENT (the "Agreement") is made and entered into as of October 16, 2017 (the ' \
+                 '“Effective  Date”)  between  Allens  Operations  Pty  Ltd  (ABN:  87  004  992  607),  with  its  office  at  Level  28, ' \
+                 'Deutsche Bank Place, 126 Philip Street, Sydney, NSW 2000 (the "Company") and Elevate Services Australia, ' \
+                 'Pty. Ltd., Level 3, 44 Martin Place, Sydney, NSW 2000 (“Elevate”).'
+
+redacted_clause = clause_redactor.redact(example_clause)
+print(redacted_clause)
 
 
